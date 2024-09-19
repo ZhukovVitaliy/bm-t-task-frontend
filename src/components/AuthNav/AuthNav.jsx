1d0aa0c3562b81832f90a434c4ff3ea9198c74c1
Zhukov Vitaliy <felex180590@gmail.com>
2024-09-19 21:53:01 +0300

    [USENSE-t-task] adds Currency converter

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <NavLink className={css.link} to="/register">
-        Register
-      </NavLink>
-      <NavLink className={css.link} to="/login">
-        Log In
-      </NavLink>
+    <div className={css.navContainer}>
+      <button className={css.burgerMenu} onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`${css.links} ${isMenuOpen ? css.showMenu : ''}`}>
+        <NavLink className={css.link} to="/register" onClick={toggleMenu}>
+          Sing In
+        </NavLink>
+        <NavLink className={css.link} to="/login" onClick={toggleMenu}>
+          Log In
+        </NavLink>
+      </nav>
     </div>
   );
 };
